0ad1f7d51b0b438c594edd909f3e08da263af01d
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-06 15:41:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Meu APP - Aula do Josir")
 st.write("Olá! Esse é o meu app do streamlit")
+st.imaghe (https://www.xbasic.org/wp-content/uploads/2022/07/Python-533x400.jpg)
 
 nome = st.text_input ("Digite seu nome:")
 if nome:
