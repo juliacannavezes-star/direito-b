f3461abffe6c23deae412abda3a3978c1677112a
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-07 11:00:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("Meu APP - Aula do Josir")
+st.title("Meu APP - Aula do Josir ⚖")
 st.write("Olá! Esse é o meu app do streamlit")
 
 nome = st.text_input ("Digite seu nome:")
 if nome:
-  st.write(Bem-vindo(a)_nome.upper())
+  st.write(nome.upper())
