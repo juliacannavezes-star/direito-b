2225ac55ded52701d4560ced2e8f81bdcd85dae6
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-07 11:44:56 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,30 +9,30 @@ nome = st.text_input("**Digite seu nome completo:**")
 if nome:
     st.success(f"**Olá, {nome.split()[0].title()}!** Seu nome tem **{len(nome)}** caracteres.")
   
-    st.header("🎨 Escolha sua Cor Favorita:")   
+    st.header("🎨 Qual a sua Cor Favorita?")   
     cor_favorita = st.selectbox(
         "Selecione uma cor para personalizar a mensagem abaixo:",
         ("Azul", "Verde", "Vermelho", "Amarelo", "Roxo"))
 
     if cor_favorita == "Azul":
-        st.info(f"Ótima escolha, {cor_favorita}! O azul é a cor do céu e da tranquilidade. 🌌")
+        st.info(f"Ótima escolha, {cor_favorita}! O azul é a cor do céu e da tranquilidade.")
     elif cor_favorita == "Verde":
-        st.success(f"Que bom, {cor_favorita}! O verde representa a natureza e esperança. 🌳")
+        st.success(f"Que bom, {cor_favorita}! O verde representa a natureza e esperança.")
     elif cor_favorita == "Vermelho":
-        st.error(f"Uau, {cor_favorita}! Uma cor de muita energia e paixão. ❤️‍🔥")
+        st.error(f"Uau, {cor_favorita}! Uma cor de muita energia e amor.")
     else:
         st.warning(f"Você escolheu **{cor_favorita}**. Uma cor vibrante! ✨")
 
     st.markdown("---")
     
     
-    st.header("📊 Aventure-se!")
+    st.header("Escala de felicidade")
 
     nivel_criatividade = st.slider(
-        'Em uma escala de 1 a 10, quão criativo está o seu app?',
+        'Em uma escala de 1 a 10, quão feliz você está?',
         1, 10, 5)
     
-    st.write(f"Você acha que o app está com nível **{nivel_criatividade}/10** de criatividade.")
+    st.write(f"Você está **{nivel_criatividade}/10** feliz.")
     
     if st.button('Clique para uma surpresa!'):
         if nivel_criatividade >= 8:
@@ -40,5 +40,5 @@ if nome:
             st.subheader("🎉 Parabéns pela sua criatividade!")
         else:
             st.snow() 
-            st.subheader("❄️ Continue explorando o Streamlit, há muito mais para aprender!")
+        
             
