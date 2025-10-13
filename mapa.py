06a5d0c06ea7ca4048ae421694adbbb9f6469c1f
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 14:58:54 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -5,9 +5,19 @@ dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
 st.dataframe(dataset)
 
 st.subheader("🗺️ Coordenadas dos países no mapa (Scatter Geo)")
-
 fig1 = px.scatter_geo(
     dataset,
     lat=dataset.latitude,
     lon=dataset.longitude,
     hover_name=dataset.nome
+)
+
+# Layout do gráfico
+fig1.update_layout(
+    showlegend=False,
+    title="Coordenadas dos países no mapa",
+    geo_scope='world'
+)
+
+# Exibir no Streamlit
+st.plotly_chart(fig1)
