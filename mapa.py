6d813430f134fba2760e97faa48de2d6f8b56495
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 15:15:00 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -6,5 +6,5 @@ st.title("Visualização de Países no Mapa")
 dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
 fig = px.choropleth(dataset, locations=dataset ['iso3'], color=dataset['nome'], hover_name=dataset['nome'])
 fig.update_layout(title="Mapa coroplético dos países!", geo_scope='world')
-st.plotly_chart(fig, use_container_with=True, theme='streamlit")
+st.plotly_chart(fig, use_container_with=True, theme='streamlit')
 
