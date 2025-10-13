84d34960c6c99ff1077757ac55a7e2b721be1be1
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 14:50:41 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,4 +1,4 @@
-import plotly.express as px
+import streamlit as pd
 fig = px.choropleth(dataset, locations=dataset ['iso3'], color=dataset['nome'], hover_name=dataset['nome'])
 fig.update_layout(title="Mapa coroplético dos países!", geo_scope='world')
 fig.show()
