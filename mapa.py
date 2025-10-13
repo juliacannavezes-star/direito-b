9db46c0db246d713db0598f6fb909b195e03e7b4
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 15:07:35 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,5 +1,7 @@
+import plotly.express as px
 import pandas as pd
 import streamlit as st
 st.title("Visualização de Países no Mapa")
 dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
-st.dataframe(dataset)
+st.dataframe(dataset
+
