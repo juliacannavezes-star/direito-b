f18fd86eb502c486220fae84f02f1f8391bbb80c
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 14:59:32 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -3,21 +3,3 @@ import streamlit as st
 st.title("Visualização de Países no Mapa")
 dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
 st.dataframe(dataset)
-
-st.subheader("🗺️ Coordenadas dos países no mapa (Scatter Geo)")
-fig1 = st.scatter_geo(
-    dataset,
-    lat=dataset.latitude,
-    lon=dataset.longitude,
-    hover_name=dataset.nome
-)
-
-# Layout do gráfico
-fig1.update_layout(
-    showlegend=False,
-    title="Coordenadas dos países no mapa",
-    geo_scope='world'
-)
-
-# Exibir no Streamlit
-st.plotly_chart(fig1)
