35e1d843cf6d4342db14cce01d0950f9eae3860c
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-13 15:16:21 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -3,8 +3,13 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 st.title("Visualização de Países no Mapa")
+
 dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
-fig = px.choropleth(dataset, locations=dataset ['iso3'], color=dataset['nome'], hover_name=dataset['nome'])
+fig = px.choropleth(dataset, 
+                    locations=dataset ['iso3'], 
+                    color=dataset['nome'], 
+                    hover_name=dataset['nome'])
 fig.update_layout(title="Mapa coroplético dos países!", geo_scope='world')
+
 st.plotly_chart(fig, use_container_with=True, theme='streamlit')
 
