89fc29e5d651a871ba96deba784351ba339df944
juliacannavezes-star <juliacannavezes@gmail.com>
2025-10-14 13:48:14 -0300

    Update deputado.py

--- deputado.py
+++ deputado.py
@@ -5,6 +5,6 @@ import plotly.express as px
 
 st.title("Visualização de Países no Mapa")
 
-dataset = pd.read_csv ('/media/uploads/deputados_2022.csv')
+dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/deputados_2022.csv')
 
 st.plotly_chart(fig, use_container_with=True, theme='streamlit')
